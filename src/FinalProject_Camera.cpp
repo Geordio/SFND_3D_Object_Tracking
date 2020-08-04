86a973ce1d792a263621f37af5119ef6ae4d6e0d
Unknown <georgehaigh@hotmail.com>
2020-08-04 11:40:43 +0100

    put some debug output

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -127,8 +127,8 @@ int main(int argc, const char *argv[])
         dataBuffer.push_back(frame);
 
         // add data buffer size management?
-
-        cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
+        cout << "-----------------------------------------------------"<< endl;
+        cout << "#1 : LOAD IMAGE INTO BUFFER done: image no: " << imgIndex<< endl;
 
         /* DETECT & CLASSIFY OBJECTS */
 
@@ -166,7 +166,7 @@ int main(int argc, const char *argv[])
         clusterLidarWithROI((dataBuffer.end() - 1)->boundingBoxes, (dataBuffer.end() - 1)->lidarPoints, shrinkFactor, P_rect_00, R_rect_00, RT);
 
         // Visualize 3D objects
-        bVis = true;
+        bVis = false;
         if (bVis)
         {
             show3DObjects((dataBuffer.end() - 1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
